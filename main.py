811fce950c54526b4cb61c1e94f656ac02228261
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-01-21 19:54:32 +0800

    Implemented GET Posts & POST Posts with global in memory array.

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
+# Post DTO with field's validation scheme
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+# Global in-memory array
+my_posts = [{ "id": 1, "title": "HOW TO - Creating CRUD API by using Fast API", "content": "TODO: Add more contents here."}, 
+            { "id": 2, "title": "HOW TO - Buy your 1st Bitcoin.", "content": "Before buying your 1st Bitcoin, you should create a new account on CryptoExchange 1st like Binance."}]
 
 @app.get("/")
 async def root():
@@ -17,9 +23,11 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"title": "HOW TO - Creating CRUD API by using Fast API", "content": "TODO: Add more contents here."}
+    return { "data": my_posts}
 
 @app.post("/posts")
-async def create_posts(newPost: Post):
-    print(newPost)
-    return { "data": newPost}
+async def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return { "data": post_dict}
